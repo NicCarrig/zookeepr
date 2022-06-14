177fd8f2d5c47789fc1091642517ade28a64f302
unknown <clemcarrig90@hotmail.com>
2022-06-14 03:56:10 -0700

    zookeeper search

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const animalsRoutes = require('../apiRoutes/animalRoutes');
+// const zookeeperRoutes = require('../apiRoutes/zookeeperRoutes');
 
 
 router.use(animalsRoutes);
+router.use(require('./zookeeperRoutes'));
 
 
 module.exports = router;
